a1b4834ad3ee37c776def433652b0e05ee7b257e
Lukas Civan <lukas.civan@gmail.com>
2023-04-28 14:38:58 +0900

    CSV class was broken down into smaller classes for readability (old version saved as well)

--- main.py
+++ main.py
@@ -3,29 +3,32 @@ from datetime import datetime
 
 
 class CSV:
-    # Class reads CSV, converts to datetime objects, and adds to dictionary
-    visits = dict()
+    # Class reads CSV and appends objects to list, removes None objects
+    csv_list = list()
     with open("csv_data20223.csv", "r") as f:
         r = csv.reader(f, delimiter=",")
         for sublist in r:
-            # None objects and blank strings are removed
             sublist = list(filter(None, sublist))
             if ' ' in sublist:
                 sublist.remove(" ")
-            for object in sublist:
-                # Dates are changed from strings to date objects
-                if object is not sublist[0]:
-                    object = datetime.strptime(object, '%Y/%m/%d')
-            visits[(sublist[0])] = sublist
-    print(visits)
+            csv_list.append(sublist)
 
 
-class Operations(CSV):
-    # Class contains all functions that will be used wih the user interface
-    # Returns total number of visits
-    count = 0
-    for key in CSV.visits:
-        if isinstance(CSV.visits[key], list):
-            # Filters out school names
-            count += len(CSV.visits[key])
-    print(count)
+class Date(CSV):
+    # Class changes string dates to datetime objects
+    for sublist in CSV.csv_list:
+        for object in sublist:
+            if object is not sublist[0]:
+                object = datetime.strptime(object, '%Y/%m/%d')
+
+
+class Dictionary(CSV):
+    # Class removes school names and appends to separate list
+    school_names = list()
+    for sublist in CSV.csv_list:
+        school_names.append(sublist[0])
+        sublist.remove(sublist[0])
+    # Class assigns key-value pairs (school, dates) using school_names
+    visits = dict(zip(school_names,CSV.csv_list))
+
+
